80a6dbc1069271a5fba60cf79f762f7e2ea47e68
rahul <rahulc2303@gmail.com>
2021-07-24 19:06:32 +0530

    new push

--- Backend/src/routes/catloc.js
+++ Backend/src/routes/catloc.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { addCategory, addLocation } = require('../controller/catloc');
+const { addCategory, addLocation,addStorePlan } = require('../controller/catloc');
 const router = express.Router();
 
 
 router.post('/category/create',addCategory)
 router.post(`/addloaction`,addLocation)
+router.post('/addStorePlan',addStorePlan)
 
 module.exports = router;
